a6e2d75eb506867a8f2e8c93a517a7e346803396
Ishaan Rai <ishaanrai18@gmail.com>
2025-01-01 16:00:55 +0530

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const uri = process.env.MONGO;
 
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 const app = express();
 
 app.listen(3000, () => {
