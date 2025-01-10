b478c13e97e064c00557402b520686f47c47cb53
Ishaan Rai <ishaanrai18@gmail.com>
2025-01-10 10:47:09 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //created a proxy to the server
+  server: {
+    proxy: {
+      '/api': {
+        target:'http://localhost:3000',
+        secure: false,
+      },
+
+    },  
+  },
   plugins: [react()],
-})
+});
